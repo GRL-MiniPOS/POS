9abc4a42affe0242c1c542c60269a75b59ff0310
leo <bjmqfg83@gmail.com>
2025-08-10 00:27:32 +0800

    refactor(frontend): remove redundant draggableMainCategoryManager component

--- apps/frontend/src/app/components/organisms/index.ts
+++ apps/frontend/src/app/components/organisms/index.ts
@@ -1,4 +1,3 @@
 export * from './sidebar/app-sidebar'
 export * from './chart/statisticalChart'
 export * from './dragAndDrop/draggableCategoryManager'
-export * from './dragAndDrop/draggableMainCategoryManager'
